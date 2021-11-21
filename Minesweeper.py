b986ae222971370084ee514f2fb64e22b0182dc8
Nghiad <nghiad@live.com>
2021-11-20 16:56:11 -0800

    custom icons

--- Minesweeper.py
+++ Minesweeper.py
@@ -6,29 +6,29 @@ def new_board(size, base):                                           #create bla
     board = [[base for column in range(size)] for row in range(size)]
     return board
 
-def plant_bombs(board, numbombs):                              #plant all bombs on board
+def plant_bombs(board, numbombs, bomb):                              #plant all bombs on board
     bombs = 0
     while bombs < numbombs:
         x = randint(0, len(board)-1)
         y = randint(0, len(board)-1)
-        board[x][y] = '*'
+        board[x][y] = bomb
         bombs += 1
     return board
 
-def check(board, pos):                               #return number of surrounding bombs
+def check(board, pos, bomb):                               #return number of surrounding bombs
     n = 0
     for row in range(max(0, (pos[0]-1)), min((pos[0]+2), len(board))):
         for column in range(max(0, (pos[1]-1)), min((pos[1]+2), len(board))):
-            if board[row][column] == '*':
+            if board[row][column] == bomb:
                 n += 1
     return n 
 
-def assign(board):                          #call check function and assignment on board
+def assign(board, bomb):                          #call check function and assignment on board
     for row in range(len(board)):                   
         for column in range(len(board)):
-            if board[row][column] == '*' or check(board,(row,column)) == 0:
+            if board[row][column] == bomb or check(board,(row,column), bomb) == 0:
                 continue
-            board[row][column] = check(board,(row,column))
+            board[row][column] = check(board,(row,column), bomb)
     return board
 
 def print_board(board):                                    #print board in proper format
@@ -67,7 +67,7 @@ def print_board(board):                                    #print board in prope
         else:
             print(ascii_letters[alpha], end=" ")                        
 
-def get_pos(playerboard):
+def get_pos(playerboard, flag):
     while True:                                             #input check for mode or pos
         command = input("Command: ").strip()
         if command == 'dig':                                        
@@ -88,7 +88,7 @@ def get_pos(playerboard):
                 except:
                     x = int(x[0])
 
-                if (playerboard[x-1][y] == 'F') or (x > len(playerboard)) or (y > len(playerboard)):
+                if (playerboard[x-1][y] == flag) or (x > len(playerboard)) or (y > len(playerboard)):
                     raise ValueError     #raise error if pos is flagged or out of bounds
 
             except:
@@ -101,37 +101,60 @@ def get_pos(playerboard):
 
                                                   #if empty, recursively dig surrounding
                                                   #check if empty from recursive digging
-def dig(pboard, mboard, pos):
-    if mboard[pos[0]][pos[1]] == '-' and pboard[pos[0]][pos[1]] != '-':
+def dig(pboard, mboard, pos, empty):
+    if mboard[pos[0]][pos[1]] == empty and pboard[pos[0]][pos[1]] != empty:
         pboard[pos[0]][pos[1]] = mboard[pos[0]][pos[1]]
         for row in range(max(0, (pos[0]-1)), min((pos[0]+2), len(pboard))):
             for column in range(max(0, (pos[1]-1)), min((pos[1]+2), len(pboard))):
-                if pboard[row][column] == '-' or (row,column) == pos:
+                if pboard[row][column] == empty or (row,column) == pos:
                     continue
-                elif mboard[row][column] == '-' and pboard[row][column] != '-':
-                    dig(pboard, mboard, (row, column))          
+                elif mboard[row][column] == empty and pboard[row][column] != empty:
+                    dig(pboard, mboard, (row, column), empty)          
                 pboard[row][column] = mboard[row][column]
     pboard[pos[0]][pos[1]] = mboard[pos[0]][pos[1]]
 
-def flag(pboard, mboard, pos, flagged):                 #win condition if flag all bombs
-    if pboard[pos[0]][pos[1]] == 'F':
-        pboard[pos[0]][pos[1]]= 0
-        if mboard[pos[0]][pos[1]] == '*':   
+def flag(pboard, mboard, pos, flagged, flag, bomb, undug):                 #win condition if flag all bombs
+    if pboard[pos[0]][pos[1]] == flag:
+        pboard[pos[0]][pos[1]]= undug
+        if mboard[pos[0]][pos[1]] == bomb:   
             flagged -=1
-    elif pboard[pos[0]][pos[1]] == 0:
-        pboard[pos[0]][pos[1]]= 'F'
-        if mboard[pos[0]][pos[1]] == '*':
+    elif pboard[pos[0]][pos[1]] == undug:
+        pboard[pos[0]][pos[1]]= flag
+        if mboard[pos[0]][pos[1]] == bomb:
             flagged +=1
     return flagged
 
-def playing(pboard):                      #lose condition if bomb appears on playerboard
+def playing(pboard, bomb):                      #lose condition if bomb appears on playerboard
     for row in range(len(pboard)):
         for column in range(len(pboard)):
-            if pboard[row][column] == '*':
+            if pboard[row][column] == bomb:
                 return False
     return True
 
 def setup(size, bombs):                                       #gets input for difficulty
+    while True:
+        difficulty = input("Input board size and number of bombs: ").strip()
+        if difficulty == '':
+            return (size, bombs)
+        try:
+            difficulty = difficulty.split()
+            size = int(difficulty[0])
+            bombs = int(difficulty[1])
+            break
+        except:
+            print ("invalid")
+    return (size, bombs)
+
+
+#Default settings
+size = 15
+bombs = 40
+empty_sym = chr(11038)  # '-'
+undug_sym = chr(9642)   #  0
+bomb_sym = chr(10694)   # '*'
+flag_sym = chr(10692)   # 'F'
+
+if __name__=='__main__':                  #game loops until win or lose condition is met
     print ()
     print ("=======================================")
     print ()
@@ -150,48 +173,27 @@ def setup(size, bombs):                                       #gets input for di
     print ("input format:", size, bombs)
     print ()
     while True:
-        difficulty = input("Input board size and number of bombs: ").strip()
-        if difficulty == '':
-            return (size, bombs)
-        try:
-            difficulty = difficulty.split()
-            size = int(difficulty[0])
-            bombs = int(difficulty[1])
-            break
-        except:
-            print ("invalid")
-    return (size, bombs)
-
-
-
-#Default settings
-size = 15
-bombs = 40
-mode = 'DIG'
-flagged = 0
-
-
-if __name__=='__main__':                  #game loops until win or lose condition is met
-    while True:
+        flagged = 0
+        mode = 'DIG'
         size, bombs = setup(size, bombs)
-        masterboard = assign(plant_bombs(new_board(size, '-'), bombs))
-        playerboard = new_board(size, 0)
-        while playing(playerboard) and flagged < bombs:
+        masterboard = assign(plant_bombs(new_board(size, empty_sym), bombs, bomb_sym), bomb_sym)
+        playerboard = new_board(size, undug_sym)
+        while playing(playerboard, bomb_sym) and flagged < bombs:
             print ()
             print_board(playerboard)
             print ()
             print ("MODE: ", mode)
             print ()
-            location = get_pos(playerboard)
+            location = get_pos(playerboard, flag_sym)
             if location == 'dig':
                 mode = 'DIG'
             elif location == 'flag':
                 mode = 'FLAG'
             else:
                 if mode == 'DIG':
-                    dig(playerboard, masterboard, location)
+                    dig(playerboard, masterboard, location, empty_sym)
                 elif mode == 'FLAG':
-                    flagged = flag(playerboard, masterboard, location, flagged)
+                    flagged = flag(playerboard, masterboard, location, flagged, flag_sym, bomb_sym, undug_sym)
     
         print_board(masterboard)
     
